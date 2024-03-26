d24d6d9eca7907f2bcb48ccddf742b7d14573218
GarifullinII <ildaridrisovich@gmail.com>
2024-03-26 21:50:23 +0400

    change layout product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -15,8 +15,25 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(color: Colors.white),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
       child: Image.network(
         imageUrl,
+        fit: BoxFit.cover,
       ),
     );
   }
