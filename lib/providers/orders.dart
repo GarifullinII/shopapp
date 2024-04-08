f2a133b6258315df5df891692e9c73c22a5b495f
GarifullinII <ildaridrisovich@gmail.com>
2024-04-08 20:16:52 +0400

    add OrderItem

---
+++ lib/providers/orders.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import '../widgets/cart_item.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  const OrderItem({
+    required this.id,
+    required this.amount,
+    required this.products,
+    required this.dateTime,
+  });
+}
+
+class Orders with ChangeNotifier {}
