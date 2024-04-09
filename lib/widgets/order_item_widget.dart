229ea15cdf0975f9eb60f696e08746508dcaf206
GarifullinII <ildaridrisovich@gmail.com>
2024-04-09 20:57:46 +0400

    change order_item_widget

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shopapp/providers/orders.dart';
 import 'package:intl/intl.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
 
   const OrderItemWidget({
@@ -11,23 +13,60 @@ class OrderItemWidget extends StatelessWidget {
   });
 
   @override
+  State<OrderItemWidget> createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  late bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: const Icon(
-                Icons.expand_more,
+              icon: Icon(
+                _expanded ? Icons.expand_less : Icons.expand_more,
               ),
-              onPressed: () {},
+              onPressed: () {
+                _expanded = !_expanded;
+                setState(() {});
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+              height: min(
+                widget.order.products.length * 20 + 100,
+                100,
+              ),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.title,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            '${product.quantity} x \$${product.price}',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
