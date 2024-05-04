aed11a626db0c0158dc0f51ee7ec63aa3d80abdf
GarifullinII <ildaridrisovich@gmail.com>
2024-05-04 14:23:30 +0400

    change OrdersScreen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,54 +4,47 @@ import 'package:shopapp/providers/orders.dart';
 import '../widgets/app_drawer_widget.dart';
 import '../widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   const OrdersScreen({super.key});
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your orders'),
       ),
       drawer: const AppDrawerWidget(),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (context, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (context, index) {
-                return Consumer(
-                  builder: (BuildContext context, value, Widget? child) {
-                    return OrderItemWidget(order: orderData.orders[index]);
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (context, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (context, index) {
+                    return Consumer(
+                      builder: (BuildContext context, value, Widget? child) {
+                        return OrderItemWidget(order: orderData.orders[index]);
+                      },
+                    );
                   },
-                );
-              },
-            ),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
